0adb0cd96c400932bd8e9e8396ace4a82b766362
awlaska <sofiassantossousa@gmail.com>
2023-02-22 15:20:42 +0000

    tudo de proj1

---
+++ 2_1_6_Proj1/dados/Artigos.sql
@@ -0,0 +1,5 @@
+insert into Artigos (id_artigo, valor, nome, descricao) values (1, 29.594, 'Lamotrigine', null);
+insert into Artigos (id_artigo, valor, nome, descricao) values (2, 75.5055, 'Cephalexin', null);
+insert into Artigos (id_artigo, valor, nome, descricao) values (3, 32.9725, 'TRICLOSAN', null);
+insert into Artigos (id_artigo, valor, nome, descricao) values (4, 4.9748, 'Raloxifene hydrochloride', null);
+insert into Artigos (id_artigo, valor, nome, descricao) values (5, 16.5712, 'Simvastatin', null);
