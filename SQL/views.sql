644f99461ff7e50dae8a0620828eba957bda19a3
awlaska <sofiassantossousa@gmail.com>
2023-03-07 15:39:00 +0000

    .

--- SQL/views.sql
+++ SQL/views.sql
@@ -1,2 +1,8 @@
 CREATE VIEW clientes_ativos as CLIATIV
-SELECT c.nome, c.
+SELECT c.nome, c.email
+FROM Cliente as c
+WHERE Subscricao.idCliente = Cliente.idCliente AND Subscricao.Estado = 'Ativa';
+
+CREATE VIEW clientes_planos as CLIPLA
+SELECT c.nome, c.email, p.numPlano
+FROM Cliente as c 
