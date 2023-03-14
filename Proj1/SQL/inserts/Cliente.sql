2bb971a786dcf8a4b43e0fbbeeae339ab5e7cde4
awlaska <sofiassantossousa@gmail.com>
2023-03-14 14:28:11 +0000

    alterar datas

--- Proj1/SQL/inserts/Cliente.sql
+++ Proj1/SQL/inserts/Cliente.sql
@@ -1,4 +1,4 @@
-insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (1, 'Britney Gration', 'bgration0@drupal.org', '4267301704', '8/27/1961', 332629286);
-insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (2, 'Agnella Botha', 'abotha1@netvibes.com', '1383149027', '12/12/1979', 430641665);
-insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (3, 'Grayce Peron', 'gperon2@hubpages.com', '3199679764', '9/24/1978', 402387390);
-insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (4, 'Pier Flott', 'pflott3@omniture.com', '4765926623', '10/5/1970', 467863723);
+insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (1, 'Britney Gration', 'bgration0@drupal.org', '4267301704', '1961-08-27', 332629286);
+insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (2, 'Agnella Botha', 'abotha1@netvibes.com', '1383149027', '1979-12-12', 430641665);
+insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (3, 'Grayce Peron', 'gperon2@hubpages.com', '3199679764', '1978-9-24', 402387390);
+insert into Cliente (idCliente, nome, email, telemovel, dtNascimento, NIF) values (4, 'Pier Flott', 'pflott3@omniture.com', '4765926623', '1970-10-5', 467863723);
