582b0ea61271b9f73fdb833d8b2041de74404bf3
awlaska <sofiassantossousa@gmail.com>
2023-05-02 13:42:24 +0100

    here you go

--- Proj2/Ginasio/proj2Gym/src/main/java/entities/Cliente.java
+++ Proj2/Ginasio/proj2Gym/src/main/java/entities/Cliente.java
@@ -1,9 +1,85 @@
 package entities;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import java.util.Date;
 
+// Table - Cliente
 @Entity
 public class Cliente {
     @Id
-    public int idCliente;
+    @GeneratedValue
+    private int idCliente;
+
+    private String nome;
+    private int idade;
+    private String email;
+    private String telemovel;
+    private int NIF;
+    private Date dtNascimento;
+    private String cPostal;
+
+    protected Cliente(){}
+
+    public Cliente(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Cliente(String nome, int idade, String email, String telemovel, int NIF, Date dtNascimento, String cPostal) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.telemovel = telemovel;
+        this.NIF = NIF;
+        this.dtNascimento = dtNascimento;
+        this.cPostal = cPostal;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelemovel() {
+        return telemovel;
+    }
+
+    public int getNIF() {
+        return NIF;
+    }
+
+    public Date getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public String getcPostal() {
+        return cPostal;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", email='" + email + '\'' +
+                ", telemovel='" + telemovel + '\'' +
+                ", NIF=" + NIF +
+                ", dtNascimento=" + dtNascimento +
+                ", cPostal='" + cPostal + '\'' +
+                '}';
+    }
 }
---
+++ Proj2/Ginasio/proj2Gym/src/main/main.java
@@ -0,0 +1,8 @@
+import Cliente;
+
+public class main {
+    public static void main(String[] args) {
+        Cliente cli = new Cliente("Jack", 30);
+        cli.toString();
+    }
+}
