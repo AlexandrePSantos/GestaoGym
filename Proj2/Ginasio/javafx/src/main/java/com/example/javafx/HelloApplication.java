00ab39a91dea8e036fe61ee0a8765895b1a10ade
awlaska <sofiassantossousa@gmail.com>
2023-05-08 12:03:19 +0100

    ainda funfa

--- Proj2/Ginasio/javafx/src/main/java/com/example/javafx/HelloApplication.java
+++ Proj2/Ginasio/javafx/src/main/java/com/example/javafx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("instrutor_dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Dashboard");
+        stage.setTitle("Instrutor");
         stage.setScene(scene);
         stage.show();
     }
--- Proj2/Ginasio/javafx/src/main/java/com/example/javafx/Rececionista/Dashboard.java
+++ Proj2/Ginasio/javafx/src/main/java/com/example/javafx/Rececionista/Dashboard.java
@@ -1,4 +1,25 @@
 package com.example.javafx.Rececionista;
 
+import javafx.fxml.FXML;
+
 public class Dashboard {
+    @FXML
+    protected void onActionAlterar(){
+        //TODO Abrir Info
+    }
+
+    @FXML
+    protected void onActionMaisInfoSubs(){
+        //TODO Abrir Planos
+    }
+
+    @FXML
+    protected void onActionMaisInfoAula(){
+        //TODO Abrir Aulas
+    }
+
+    @FXML
+    protected void onActionCriarCliente(){
+        //TODO Abrir Criar Cliente
+    }
 }
