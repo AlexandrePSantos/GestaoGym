31ce5998ca6ff8f9f3b52f189435bd3a3eb60db1
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-05-09 16:36:18 +0100

    as

--- Proj2/Ginasio/javafx/src/main/java/repository/entities/Equipamento.java
+++ Proj2/Ginasio/javafx/src/main/java/repository/entities/Equipamento.java
@@ -1,4 +1,4 @@
-package entities;
+package repository.entities;
 
 import javax.persistence.*;
 import java.util.LinkedHashSet;
--- Proj2/Ginasio/javafx/src/main/java/repository/entities/Linhaparticipante.java
+++ Proj2/Ginasio/javafx/src/main/java/repository/entities/Linhaparticipante.java
@@ -1,4 +1,4 @@
-package entities;
+package repository.entities;
 
 import javax.persistence.*;
 
