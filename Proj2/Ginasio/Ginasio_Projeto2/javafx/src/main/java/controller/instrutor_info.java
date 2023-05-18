9e212cd4940d95f5d895c1983b8b50880c2da6aa
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-05-18 15:49:45 +0100

    exit

--- Proj2/Ginasio/Ginasio_Projeto2/javafx/src/main/java/controller/instrutor_info.java
+++ Proj2/Ginasio/Ginasio_Projeto2/javafx/src/main/java/controller/instrutor_info.java
@@ -41,4 +41,19 @@ public class instrutor_info {
         Stage stageAtual = (Stage) source.getScene().getWindow();
         stageAtual.close();
     }
+
+    @FXML
+    protected void onActionExit(ActionEvent event) throws IOException {
+        //DONE Abrir instrutor_aulas
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("../Ginasio_Projeto2/javafx/src/main/resources/com/example/javafx/_login.fxml").toURI().toURL());
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+
+        Node source = (Node) event.getSource();
+        Stage stageAtual = (Stage) source.getScene().getWindow();
+        stageAtual.close();
+    }
 }
