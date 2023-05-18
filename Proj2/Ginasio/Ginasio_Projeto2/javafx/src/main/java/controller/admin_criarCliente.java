3ea41a180ee1f9a270cf34b4203b95b31bfb295a
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-05-18 22:24:52 +0100

    more changes

--- Proj2/Ginasio/Ginasio_Projeto2/javafx/src/main/java/controller/admin_criarCliente.java
+++ Proj2/Ginasio/Ginasio_Projeto2/javafx/src/main/java/controller/admin_criarCliente.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -31,6 +32,21 @@ public class admin_criarCliente {
         FXMLLoader fxmlLoader = new FXMLLoader(new File("../Ginasio_Projeto2/javafx/src/main/resources/com/example/javafx/rececionista_dashboard.fxml").toURI().toURL());
         Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
         stage.setTitle("Rececionista: Dashboard");
+        stage.setScene(scene);
+        stage.show();
+
+        Node source = (Node) event.getSource();
+        Stage stageAtual = (Stage) source.getScene().getWindow();
+        stageAtual.close();
+    }
+
+    @FXML
+    protected void onActionExit(MouseEvent event) throws IOException {
+        //DONE Abrir instrutor_aulas
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("../Ginasio_Projeto2/javafx/src/main/resources/com/example/javafx/_login.fxml").toURI().toURL());
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
 
