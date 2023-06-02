fc50a4dd92ff88cd6fa2357b7fd4feb73451457d
awlaska <sofiassantossousa@gmail.com>
2023-06-02 16:55:42 +0100

    agora funfa

--- Proj2/Ginasio/Ginasio_Projeto2/javafx/src/main/java/com/example/javafx/App.java
+++ Proj2/Ginasio/Ginasio_Projeto2/javafx/src/main/java/com/example/javafx/App.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("_login.fxml"));
+//       FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("_login.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/Fxml/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
