b671e64947add149748b3712928bf5820e5a9b34
awlaska <sofiassantossousa@gmail.com>
2023-06-05 12:30:54 +0100

    começo da web

--- Proj2/Ginasio/Ginasio_Projeto2/spring/src/main/java/com/example/Application.java
+++ Proj2/Ginasio/Ginasio_Projeto2/spring/src/main/java/com/example/Application.java
@@ -3,11 +3,16 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 
 }
