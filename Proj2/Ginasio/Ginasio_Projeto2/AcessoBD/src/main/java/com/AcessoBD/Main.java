50e538552cfa58bc4e1773b2e11ad5a3344ca8b0
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-06-06 16:03:55 +0100

    idk

--- Proj2/Ginasio/Ginasio_Projeto2/AcessoBD/src/main/java/com/AcessoBD/Main.java
+++ Proj2/Ginasio/Ginasio_Projeto2/AcessoBD/src/main/java/com/AcessoBD/Main.java
@@ -18,32 +18,6 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-//        // Create a new Cliente entity
-//        Cliente cliente = new Cliente();
-//        cliente.setNome("John Doe3");
-//        cliente.setEmail("johndoe@example.com");
-//        cliente.setTelemovel("555-1234");
-//
-//        // Persist the new Cliente entity
-//        em.persist(cliente);
-
-        // Commit the transaction
-//        tx.commit();
-//
-//        // Close the EntityManager and the EntityManagerFactory
-//        em.close();
-//        emf.close();
-
-//        // Create a new Funcionario entity
-//        Login login = new Login();
-//        Funcionario func = new Funcionario();
-//        login.setId("Inst1");
-//        login.setPassword("123456");
-//        func.setLogin(login);
-//        // Persist the new Cliente entity
-//        em.persist(login);
-//        em.persist(func);
-
         // Commit the transaction
         tx.commit();
 
