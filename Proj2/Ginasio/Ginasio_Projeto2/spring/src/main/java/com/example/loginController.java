01b7a637352408b775978eeed21d3ce527c8706e
awlaska <sofiassantossousa@gmail.com>
2023-06-08 15:54:07 +0100

    cores

--- Proj2/Ginasio/Ginasio_Projeto2/spring/src/main/java/com/example/loginController.java
+++ Proj2/Ginasio/Ginasio_Projeto2/spring/src/main/java/com/example/loginController.java
@@ -2,26 +2,49 @@ package com.example;
 
 import com.AcessoBD.DAO.ClienteDAO;
 import com.AcessoBD.repository.entities.Cliente;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.asm.Label;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class loginController {
-    ClienteDAO cd = new ClienteDAO();
 
-    @PostMapping("/redirectToDashboard")
-    public String redirectToDashboard(@RequestParam("email") String email, @RequestParam("password") String password) {
-        Cliente c = cd.getByEmail(email);
+    public Label email;
+    public Label password;
 
-        if (c != null && c.getPassword().equals(password)) {
-            return "redirect:/loadPerfil?iduser=" + c.getIdCliente();
-        } else {
-            // handle error case
-            return "redirect:/error";
+    Cliente c = new Cliente();
+    ClienteDAO cDAO = new ClienteDAO();
+
+    @PostMapping("/templates/dashboard.html")
+    public String login(String email, String password, HttpServletResponse response) throws IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+
+        // Verificar se os campos de email e senha estão vazios
+        if (email.isEmpty() || password.isEmpty()) {
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<head><title>Alerta</title></head>");
+            out.println("<body>");
+            out.println("<script>alert('Dados incorretos. Por favor, verifique os campos novamente.');</script>");
+            out.println("</body>");
+            out.println("</html>");
+            return "/static/login.html";
+            // Terminar o método aqui para evitar exceções desnecessárias
         }
+        else {
+            c = cDAO.getByEmail(email);
+        }
+        return "/templates/dashboard.html?user=" + c.getIdCliente();
+    }
+
+    @RequestMapping("/login.html")
+    public String showLoginPage() {
+        return "/static/login.html";
     }
 }
